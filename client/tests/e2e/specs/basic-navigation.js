3a2d902bf465270d2c643ed89ed72c2afbecfd54
Marcv Verdaguer <marc.verdaguer.ginestera@gmail.com>
2019-07-25 10:42:24 +0200

    E2e tests for navigation using the menu links.

--- client/tests/e2e/specs/basic-navigation.js
+++ client/tests/e2e/specs/basic-navigation.js
@@ -1,5 +1,3 @@
-// https://docs.cypress.io/api/introduction/api.html
-
 describe('Main route navigation and dynamic menu', () => {
   const checkMenu = (selectedElementsCount, text) => {
     cy.get('div.desktop-header')
@@ -16,101 +14,118 @@ describe('Main route navigation and dynamic menu', () => {
     }
   };
 
+  const checkTitle = (cssSelector, title) => {
+    cy.get(`${cssSelector} h1`)
+        .contains(title);
+  };
+
+  const createItCase = itCase => {
+    it(itCase.title, () => {
+      cy.visit(itCase.route);
+
+      checkMenu(1), itCase.menuTitle;
+      checkTitle(itCase.cssClass, itCase.h1Title);
+    });
+  }
+
   it('Visits the home page', () => {
     cy.visit('/');
-    cy.get('.main-page h1')
-      .should('exist');
 
     checkMenu(0);
+    checkTitle('.main-page', 'Shop');
   });
 
-  it('Visits the contact page', () => {
-    cy.visit('/contact');
-    cy.get('.contact-page h1')
-      .should('exist');
+  const basicRoutesParams = [
+    {title: 'Contact page', route: '/contact', menuTitle: 'Contact', cssClass: '.contact-page', h1Title: 'Contact'},
+    {title: 'Admin (item) page', route: '/admin', menuTitle: 'Admin', cssClass: '.main__page', h1Title: 'Products'},
+    {title: 'Admin (category) page', route: '/admin_categories', menuTitle: 'Admin', cssClass: '.main__page', h1Title: 'Categories'},
+    {title: 'Admin (supplier) page', route: '/admin_suppliers', menuTitle: 'Admin', cssClass: '.main__page', h1Title: 'Providers'},
+    {title: 'Admin (home images) page', route: '/admin_home_images', menuTitle: 'Admin', cssClass: '.main__page', h1Title: 'Home images'},
+  ];
 
-    checkMenu(1), 'Contact';
-  });
-
-  it('Visits the admin (item) page', () => {
-    cy.visit('/admin');
-    cy.get('.main__page h1')
-      .contains('Products');
-
-    checkMenu(1), 'Admin';
-  });
-
-  it('Visits the admin (category) page', () => {
-    cy.visit('/admin_categories');
-    cy.get('.main__page h1')
-      .contains('Categories');
-
-    checkMenu(1), 'Admin';
-  });
-
-  it('Visits the admin (suppliers) page', () => {
-    cy.visit('/admin_suppliers');
-    cy.get('.main__page h1')
-      .contains('Providers');
-
-    checkMenu(1), 'Admin';
-  });
-
-  it('Visits the admin (Home images) page', () => {
-    cy.visit('/admin_home_images');
-    cy.get('.main__page h1')
-      .contains('Home images');
-
-    checkMenu(1), 'Admin';
+  basicRoutesParams.forEach(itCase => {
+    createItCase(itCase);
   });
 
   describe('Product pages navigation', () => {
-    it('Vegetables page', () => {
-      cy.visit('/products/vegetables');
-      cy.get('.items-list-page h1')
-        .contains('vegetables');
+    const testParams = [
+      {title: 'Vegetables page', route: '/products/vegetables', menuTitle: 'vegetables', cssClass: '.items-list-page', h1Title: 'vegetables'},
+      {title: 'Fruit page', route: '/products/fruit', menuTitle: 'fruit', cssClass: '.items-list-page', h1Title: 'fruit'},
+      {title: 'Carbohydrates page', route: '/products/carbohydrates', menuTitle: 'carbohydrates', cssClass: '.items-list-page', h1Title: 'carbohydrates'},
+      {title: 'Grains page', route: '/products/grains', menuTitle: 'carbohydrates', cssClass: '.items-list-page', h1Title: 'grains'},
+      {title: 'Legumes page', route: '/products/legumes', menuTitle: 'carbohydrates', cssClass: '.items-list-page', h1Title: 'legumes'},
+      {title: 'Beans page', route: '/products/beans', menuTitle: 'carbohydrates', cssClass: '.items-list-page', h1Title: 'beans'},
+    ];
 
+    testParams.forEach(itCase => {
+      createItCase(itCase);
+    });
+  });
+
+  describe('Menu navigation', () => {
+    it('Clicking the menu items you visit the selected page.', () => {
+      cy.visit('/');
+     
+      cy.get('div.desktop-header__item')
+        .eq(0) 
+        .click();
+      checkMenu(1), 'carbohydrates';
+      checkTitle('.items-list-page', 'carbohydrates');
+
+      // ---------------------
+      // Carbohydrates subcategories.
+      cy.get('div.desktop-header__item')
+        .eq(0) 
+        .trigger('mouseover');
+
+      cy.get('div.desktop-header__item')
+        .eq(0) 
+        .get('ul > li > a')
+        .eq(0)
+        .click();
+      checkMenu(1), 'carbohydrates';
+      checkTitle('.items-list-page', 'grains');
+
+      cy.get('div.desktop-header__item')
+        .eq(0) 
+        .get('ul > li > a')
+        .eq(1)
+        .click();
+      checkMenu(1), 'carbohydrates';
+      checkTitle('.items-list-page', 'legumes');
+
+      cy.get('div.desktop-header__item')
+        .eq(0) 
+        .get('ul > li > a')
+        .eq(2)
+        .click();
+      checkMenu(1), 'carbohydrates';
+      checkTitle('.items-list-page', 'beans');
+      // ---------------------
+
+      cy.get('div.desktop-header__item')
+        .eq(1) 
+        .click();
       checkMenu(1), 'vegetables';
-    });
-
-    it('Fruit page', () => {
-      cy.visit('/products/fruit');
-      cy.get('.items-list-page h1')
-        .contains('fruit');
-
+      checkTitle('.items-list-page', 'vegetables');
+      
+      cy.get('div.desktop-header__item')
+        .eq(2) 
+        .click();
       checkMenu(1), 'fruit';
-    });
-
-    it('Carbohydrates page', () => {
-      cy.visit('/products/carbohydrates');
-      cy.get('.items-list-page h1')
-        .contains('carbohydrates');
-
-      checkMenu(1), 'carbohydrates';
-    });
-
-    it('Grains page', () => {
-      cy.visit('/products/grains');
-      cy.get('.items-list-page h1')
-        .contains('grains');
-
-      checkMenu(1), 'carbohydrates';
-    });
-
-    it('Legumes page', () => {
-      cy.visit('/products/legumes');
-      cy.get('.items-list-page h1')
-        .contains('legumes');
-
-      checkMenu(1), 'carbohydrates';
-    });
-
-    it('Beans page', () => {
-      cy.visit('/products/beans');
-      cy.get('.items-list-page h1')
-        .contains('beans');
-
-      checkMenu(1), 'carbohydrates';
+      checkTitle('.items-list-page', 'fruit');
+      
+      cy.get('div.desktop-header__item')
+        .eq(3) 
+        .click();
+      checkMenu(1), 'contact';
+      checkTitle('.contact-page', 'Contact');
+      
+      cy.get('div.desktop-header__item')
+        .eq(4) 
+        .click();
+      checkMenu(1), 'admin';
+      checkTitle('.main__page', 'Products');
     });
   });
 });
